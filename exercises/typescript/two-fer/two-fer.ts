0d662405c909548393aeb425409619a007dcb60c
Mihailov Vadim <mihailov.vad@gmail.com>
2019-10-02 20:10:40 +0400

    Implemented Two-Fer exercise

---
+++ exercises/typescript/two-fer/two-fer.ts
@@ -0,0 +1,9 @@
+export default class TwoFer {
+    public static twoFer(name?: string): string {
+        if (name === undefined) {
+            return "One for you, one for me."
+        } else {
+    	    return `One for ${name}, one for me.`
+        }
+    }
+}
